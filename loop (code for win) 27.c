e4189fc9468f40f60aa9778f0443fe9bdc4a1cad
Khadija Akter Lima <38761302+lima21bd@users.noreply.github.com>
2018-05-24 02:37:08 +0600

    Add files via upload

---
+++ loop (code for win) 27.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int i,num,x=0;
+    scanf ("%d",&num);
+    for (i=2;i<=num/2;i++)
+    {
+        if (num%i == 0)
+        {
+            x=1;
+            break;
+        }
+        else
+            x=0;
+    }
+    if (x==0)
+        printf ("%d is prime",num);
+}
