daca4cca0b78021699d3722f40f4a02c9b8d8189
Khadija Akter Lima <38761302+lima21bd@users.noreply.github.com>
2018-05-24 03:22:07 +0600

    Add files via upload

---
+++ loop (float input).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float a = 10.0;
+    while (a<=10.5){
+        printf ("Raindrops on roses...");
+        printf ("...and whiskers on kittens\n");
+        a = a+.01;
+    }
+    return 0;
+}
