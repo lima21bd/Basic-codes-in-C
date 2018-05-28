e962ab3fa6d9f0bbd9fab3fd97783db49fff528c
Khadija Akter Lima <38761302+lima21bd@users.noreply.github.com>
2018-05-28 22:25:30 +0600

    Add files via upload

---
+++ string 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    char country[] = {'B','a','n','g','l','a','d','e','s','h','\0'};
+    int i , length;
+    printf ("%s\n",country);
+    length = 10;
+    for (i=0;i<length;i++)
+    {
+        if (country[i]>=97 && country[i]<=122)
+            country[i] = 'A' + (country[i]-'a');
+    }
+    printf ("%s\n",country);
+}
