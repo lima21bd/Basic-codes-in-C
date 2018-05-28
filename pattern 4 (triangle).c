c9233ea98eafa61ffa85e951a1ae7e9c21afc981
Khadija Akter Lima <38761302+lima21bd@users.noreply.github.com>
2018-05-28 22:21:08 +0600

    Add files via upload

---
+++ pattern 4 (triangle).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int row,column,space,end;
+    scanf ("%d",&end);
+    for(row=0;row<end;row++)
+    {
+        for (space=end-1;space>row;space--)
+            printf (" ");
+        for (column=0;column<row+1;column++)
+            printf ("*");
+        printf ("\n");
+    }
+}
